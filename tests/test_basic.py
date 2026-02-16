88907f0b0c7df2b9ea8750890fa6d5cbf6743bb2
anakin87 <stefanofiorucci@gmail.com>
2026-02-16 11:02:23 +0100

    test more components, incl. inheritance

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -8,19 +8,19 @@ import pytest
 def cleanup_files():
     yield
 
-    if os.path.exists("preprocessors_api.md"):
-        os.remove("preprocessors_api.md")
+    if os.path.exists("generators_api.md"):
+        os.remove("generators_api.md")
 
 
 def test_can_build_preprocessors_api(cleanup_files):
-    subprocess.run(["pydoc-markdown", "tests/test_files/preprocessors_api.yml"], check=True)
+    subprocess.run(["pydoc-markdown", "tests/test_files/generators_api.yml"], check=True)
 
-    assert os.path.exists("preprocessors_api.md")
+    assert os.path.exists("generators_api.md")
 
-    with open("preprocessors_api.md", "r") as f:
+    with open("generators_api.md", "r") as f:
         content = f.read()
 
-    with open("tests/test_files/preprocessors_api_output.md", "r") as f:
+    with open("tests/test_files/expected_generators_api.md", "r") as f:
         expected_content = f.read()
 
     assert content == expected_content
